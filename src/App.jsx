617b915e25b8c12f63a6ba039bf4ffd5c635fb0f
edwardyq67 <edwardyq67@gmail.com>
2024-06-20 11:54:45 -0500

    actualizacion1

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,43 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Inicio from './page/Inicio';
 import Nosotros from './page/Nosotros';
 import Footer from './components/Footer';
+import Loadding from './loadding/Loadding';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [showLoading, setShowLoading] = useState(true);
+
+  useEffect(() => {
+    // Verificar si ya se ha mostrado la carga
+    const hasShownLoading = localStorage.getItem('hasShownLoading');
+
+    if (hasShownLoading) {
+      setShowLoading(false);
+    } else {
+      // Mostrar la carga por 2 segundos
+      const timer = setTimeout(() => {
+        setShowLoading(false);
+        localStorage.setItem('hasShownLoading', 'true');
+      }, 2000);
+
+      return () => clearTimeout(timer);
+    }
+  }, []);
 
   return (
     <div className="">
       <HashRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/nosotros" element={<Nosotros />} />
-      </Routes>
-      <Footer/>
-    </HashRouter>
+        <NavBar />
+        {showLoading && <Loadding />}
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/nosotros" element={<Nosotros />} />
+        </Routes>
+        <Footer />
+      </HashRouter>
     </div>
-
-    
   );
 }
 
